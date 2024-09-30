a7cad2a7c9417354641d5b9a9b1a52ebc41063e6
MohammadFazaluddin <fazaluddin2806@gmail.com>
2024-09-30 18:07:38 +0530

    removed neighbours class

--- Grid.cs
+++ Grid.cs
@@ -4,15 +4,12 @@ public class Grid : IGrid
 {
     private HashSet<Cell> _liveCells;
     private ISimulationRules _rules;
-    private INeighbour _neighbour;
 
-    public Grid(List<Cell> initial, ISimulationRules rules, INeighbour neighbour)
+    public Grid(List<Cell> initial, ISimulationRules rules)
     {
         _liveCells = new(initial);
 
         _rules = rules;
-
-        _neighbour = neighbour;
     }
 
     public void UpdateNextGeneration()
@@ -30,11 +27,11 @@ public class Grid : IGrid
     {
         var newLiveCells = new HashSet<Cell>();
 
-        var neighbourCells = _neighbour.GenerateNeighbours(currentLiveCells);
+        var neighbourCells = GenerateNeighbours(currentLiveCells);
 
         foreach (var cell in neighbourCells)
         {
-            var liveNeighbours = _neighbour.GetLiveNeighborCount(cell, currentLiveCells, neighbourCells);
+            var liveNeighbours = GetLiveNeighborCount(cell, currentLiveCells, neighbourCells);
             bool isAlive = currentLiveCells.Contains(cell);
 
             if (rules.CheckCellState(isAlive, liveNeighbours))
@@ -46,5 +43,62 @@ public class Grid : IGrid
         return newLiveCells;
     }
 
+    public HashSet<Cell> GenerateNeighbours(HashSet<Cell> initialLiveCells)
+    {
+        var neighbourCells = new HashSet<Cell>();
+
+        foreach (var cell in initialLiveCells)
+        {
+            neighbourCells.Add(cell);
+            foreach (var neighbour in GetNeighbours(cell))
+            {
+                if (!neighbourCells.Contains(neighbour))
+                    neighbourCells.Add(neighbour);
+
+            }
+        }
+
+        return neighbourCells;
+    }
+
+    private IEnumerable<Cell> GetNeighbours(Cell cell, HashSet<Cell>? currentCells = null)
+    {
+        int[] offsets = { -1, 0, 1 };
+        foreach (int dx in offsets)
+        {
+            foreach (int dy in offsets)
+            {
+                int posX = cell.Position.X + dx;
+                int posY = cell.Position.Y + dy;
+                if (posX < 0 || posY < 0) continue;
+                if (dx == 0 && dy == 0) continue;
+
+                if (currentCells is not null)
+                {
+                    Cell? neighbourCell = currentCells.FirstOrDefault(c => c.Position.Equals(new Coordinate(posX, posY)));
+
+                    if (neighbourCell is not null)
+                        yield return neighbourCell;
+
+                    continue;
+                }
+                else
+                    yield return new Cell(posX, posY, false);
+            }
+        }
+    }
+
+
+
+    public int GetLiveNeighborCount(Cell cell, HashSet<Cell> liveCells, HashSet<Cell> currentCells)
+    {
+        int liveCount = 0;
+
+        foreach (var neighbour in GetNeighbours(cell, currentCells))
+            if (liveCells.Contains(neighbour))
+                liveCount++;
+
+        return liveCount;
+    }
 }
 
--- INeighbours.cs
+++
@@ -1,9 +0,0 @@
-namespace GameOfLife;
-
-public interface INeighbour
-{
-    HashSet<Cell> GenerateNeighbours(HashSet<Cell> initialLiveCells);
-
-    int GetLiveNeighborCount(Cell cell, HashSet<Cell> liveCells, HashSet<Cell> currentCells);
-}
-
